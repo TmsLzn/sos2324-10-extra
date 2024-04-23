19fe5e0fa879a142cee22c7a3416c599da0d2e8a
TmsLzn <tomasito.lozano@gmail.com>
2024-04-23 18:18:11 +0200

    H

--- front/src/routes/analytics/+page.svelte
+++ front/src/routes/analytics/+page.svelte
@@ -4,64 +4,29 @@
 
 	let API_TLR = '/api/v2/vehicles-stock';
 	let API_MRF = '/api/v2/gdp-growth-rates';
-	let API_ASC = '/api/v2/tourisms-per-age';
-	let API_ASB = '/api/v2/cars-by-motor';
+
 	let errorMsg = '';
 	let exitMsg = '';
 	let datos1,
-		datos2,
-		datos3,
-		datos4 = {};
+		datos2 = {};
 
 	if (dev) {
 		API_TLR = 'http://localhost:8080' + API_TLR;
 		API_MRF = 'http://localhost:8080' + API_MRF;
-		API_ASC = 'http://localhost:8080' + API_ASC;
-		API_ASB = 'http://localhost:8080' + API_ASB;
+		
 	}
 
 	onMount(async () => {
 		await loadAllData();
 		datos1 = await getVehicles();
 		datos2 = await getGDP();
-		datos3 = await getTourisms();
-		datos4 = await getCars();
-		datos4 = replaceeGeo(datos4);
-		datos3 = replaceeGeo(datos3);
-		//console.log('Datos ASC parseados: ' + JSON.stringify(datos3));
-		//console.log('Datos ASB parseados: ' + JSON.stringify(datos4));
-		//console.log('DATOS MRF Crudos: ' + JSON.stringify(datos2));
 		datos2 = replaceGeo(datos2);
-		//console.log('DATOS MRF: ' + JSON.stringify(datos2));
-		let datos = unificarBD(datos1, datos2, datos3, datos4);
+
+		let datos = unificarBD(datos1, datos2);
 		console.log('DATOS COMUNES: ' + JSON.stringify(datos));
 		datos = getEstadisticas(datos);
-		//console.log('DATOS TRATADOS: ' + JSON.stringify(datos));
 		getChart(datos);
 	});
-
-	//Funciones para obtener los datos en la BD
-	/*
-	async function getInitialData() {
-		try {
-			if (datos1.length === 0) {
-				let response = await fetch(API_TLR + '/loadInitialData', {
-					method: 'GET'
-				});
-
-				if (response.ok) {
-					getVehicles();
-					alert('Datos Cargados Correctamente');
-				} else {
-					errorMsg = 'La base de datos no está vacía';
-				}
-			} else {
-				errorMsg = 'La base de datos no está vacía';
-			}
-		} catch (error) {
-			errorMsg = error;
-		}
-	}*/
 
 	async function getInitialData() {
 		try {
@@ -106,23 +71,6 @@
 		}
 	}
 
-	/*
-	async function getInitialGDP() {
-		try {
-			if (datos2.length === 0) {
-				let response = await fetch(API_MRF + '/loadInitialData', {
-					method: 'GET'
-				});
-
-				if (response.ok) {
-					exitoMsg = 'Datos cargados correctamente';
-					errorMsg = '';
-				}
-			}
-		} catch (e) {
-			errorMsg = e;
-		}
-	}*/
 
 	async function getInitialGDP() {
 		try {
@@ -163,142 +111,12 @@
 		}
 	}
 
-	///////
-	/*
-	async function loadInitialDataASC() {
-		try {
-			if (datos3.length === 0) {
-				let response = await fetch(API_ASC + '/loadInitialData', {
-					method: 'GET'
-				});
-
-				if (response.ok) {
-					getTourisms();
-					exitMsg = 'Datos Cargados Correctamente';
-				} else {
-					errorMsg = 'La base de datos no está vacía';
-				}
-			} else {
-				errorMsg = 'La base de datos no está vacía';
-			}
-		} catch (error) {
-			errorMsg = error;
-		}
-	}*/
-	async function loadInitialDataASC() {
-		try {
-			let response = await fetch(API_ASC + '/loadInitialData', {
-				method: 'GET'
-			});
-			if (response.ok) {
-				//getTourisms();
-				exitMsg = 'Datos Cargados Correctamente';
-			} else {
-				errorMsg = 'La base de datos no está vacía';
-			}
-		} catch (error) {
-			errorMsg = error;
-		}
-	}
-
-	async function getTourisms() {
-		try {
-			await loadInitialDataASC();
-			let response = await fetch(`${API_ASC}?limit=10000`, {
-				method: 'GET',
-				headers: {
-					'Cache-Control': 'no-cache',
-					Pragma: 'no-cache'
-				}
-			});
-
-			if (response.ok) {
-				let data = await response.json();
-				//console.log('DATOS ASC: ' + JSON.stringify(data));
-				return data;
-			} else {
-				if (response.status == 404) {
-					errorMsg = 'No hay datos3 en la base de datos3';
-				} else {
-					errorMsg = `Error ${response.status}: ${response.statusText}`;
-				}
-			}
-		} catch (e) {
-			errorMsg = e;
-		}
-	}
-	///////
-	/*
-	async function loadInitialCars() {
-		try {
-			if (datos4.length === 0) {
-				let response = await fetch(API_ASB + '/loadInitialData', {
-					method: 'GET'
-				});
-
-				if (response.ok) {
-					getCars();
-					alert('Datos Cargados Correctamente');
-				} else {
-					errorMsg = 'La base de datos no está vacía';
-				}
-			} else {
-				errorMsg = 'La base de datos no está vacía';
-			}
-		} catch (error) {
-			errorMsg = error;
-		}
-	}*/
-
-	async function loadInitialCars() {
-		try {
-			let response = await fetch(API_ASB + '/loadInitialData', {
-				method: 'GET'
-			});
-
-			if (response.ok) {
-				exitMsg = 'Datos cartados correctamente';
-			} else {
-				errorMsg = 'La base de datos no está vacía';
-			}
-		} catch (error) {
-			errorMsg = error;
-		}
-	}
-
-	async function getCars() {
-		try {
-			await loadInitialCars();
-			let response = await fetch(`${API_ASB}?limit=10000`, {
-				method: 'GET',
-				headers: {
-					'Cache-Control': 'no-cache',
-					Pragma: 'no-cache'
-				}
-			});
-
-			if (response.ok) {
-				let data = await response.json();
-				return data;
-			} else {
-				if (response.status == 404) {
-					errorMsg = 'No hay datos4 en la base de datos3';
-				} else {
-					errorMsg = `Error ${response.status}: ${response.statusText}`;
-				}
-			}
-		} catch (e) {
-			errorMsg = e;
-		}
-	}
-
+	
 	//Cargar todos los datos
 	async function loadAllData() {
 		try {
 			await getInitialData();
 			await getInitialGDP();
-			await loadInitialCars();
-			await loadInitialDataASC();
 		} catch (error) {
 			errorMsg = error;
 		}
@@ -346,83 +164,29 @@
 		});
 	}
 
-	function replaceeGeo(data3) {
-		const countryNames = {
-			DE: 'Alemania',
-			EE: 'Estonia',
-			IE: 'Irlanda',
-			EL: 'Grecia',
-			ES: 'España',
-			BG: 'Bulgaria',
-			CZ: 'República Checa',
-			FR: 'Francia',
-			HR: 'Croacia',
-			IT: 'Italia',
-			CY: 'Chipre',
-			LV: 'Letonia',
-			LT: 'Lituania',
-			LU: 'Luxemburgo',
-			HU: 'Hungría',
-			MT: 'Malta',
-			NL: 'Países Bajos',
-			AT: 'Austria',
-			PL: 'Polonia',
-			PT: 'Portugal',
-			RO: 'Rumania',
-			SI: 'Eslovenia',
-			SK: 'Eslovaquia',
-			FI: 'Finlandia',
-			SE: 'Suecia',
-			IS: 'Islandia',
-			LI: 'Liechtenstein',
-			NO: 'Noruega',
-			CH: 'Suiza',
-			UK: 'Reino Unido',
-			BA: 'Bosnia y Herzegovina',
-			ME: 'Montenegro',
-			MD: 'Moldavia',
-			MK: 'Macedonia del Norte',
-			AL: 'Albania',
-			RS: 'Serbia',
-			TR: 'Turquía',
-			XK: 'Kosovo',
-			GE: 'Georgia'
-		};
 
-		return data3.map((item) => {
-			return {
-				...item,
-				geo: countryNames[item.geo] || item.geo // Si el país no está en la lista, deja el valor original
-			};
-		});
-	}
 
 	//Creamos función que unifique datos
-	function unificarBD(data1, data2, data3, data4) {
+	function unificarBD(data1, data2) {
 		// Crear conjuntos de países únicos para cada conjunto de datos
 		const geoSet1 = new Set(data1.map((item) => item.geo));
 		const geoSet2 = new Set(data2.map((item) => item.geo));
-		const geoSet3 = new Set(data3.map((item) => item.geo));
-		const geoSet4 = new Set(data4.map((item) => item.geo));
 
 		// Encontrar la intersección de países comunes a todas las bases de datos
 		const commonGeoSet = new Set(
-			[...geoSet1].filter((geo) => geoSet2.has(geo) && geoSet3.has(geo) && geoSet4.has(geo))
+			[...geoSet1].filter((geo) => geoSet2.has(geo))
 		);
 
 		// Filtrar datos para incluir solo países comunes
 		const filteredData1 = data1.filter((item) => commonGeoSet.has(item.geo));
 		const filteredData2 = data2.filter((item) => commonGeoSet.has(item.geo));
-		const filteredData3 = data3.filter((item) => commonGeoSet.has(item.geo));
-		const filteredData4 = data4.filter((item) => commonGeoSet.has(item.geo));
 
 		// Combinar todos los datos filtrados
-		const combinedData = [...filteredData1, ...filteredData2, ...filteredData3, ...filteredData4];
+		const combinedData = [...filteredData1, ...filteredData2];
 
 		return combinedData;
 	}
 
-	// Función para crear la estadística
 	// Función para crear la estadística
 	function getEstadisticas(datos) {
 		const countryData = datos.reduce((acc, curr) => {
@@ -437,14 +201,6 @@
 			if (curr.flights_passangers) {
 				acc[curr.geo].deathsInFlights += curr.flights_passangers;
 			}
-			// Volgdp
-			if (curr.volgdp) {
-				acc[curr.geo].volgdp = curr.volgdp;
-			}
-			// road_deaths_per_million_inhabitants
-			if (curr.road_deaths_per_million_inhabitants) {
-				acc[curr.geo].road_deaths_per_million_inhabitants += curr.road_deaths_per_million_inhabitants;
-			}
 			return acc;
 		}, {});
 
@@ -452,9 +208,7 @@
 			([country, { pib, deathsInFlights, volgdp ,road_deaths_per_million_inhabitants}]) => ({
 				country: country,
 				pib: pib,
-				deathsInFlights: deathsInFlights,
-				volgdp: volgdp, // Incluir el volgdp
-				road_deaths_per_million_inhabitants: road_deaths_per_million_inhabitants
+				deathsInFlights: deathsInFlights
 			})
 		);
 
@@ -468,14 +222,6 @@
 				{
 					name: 'Muertes en Aviones',
 					data: sortedData.map((item) => item.deathsInFlights)
-				},
-				{
-					name: 'VolGDP',
-					data: sortedData.map((item) => item.volgdp) // Nueva serie para VolGDP
-				},
-				{
-					name: 'Muertes en carretera por millón de habitantes',
-					data: sortedData.map((item) => item.road_deaths_per_million_inhabitants)
 				}
 			]
 		};
@@ -509,19 +255,6 @@
 						text: 'Muertes en Aviones'
 					},
 					opposite: true
-				},
-				{
-					min: 0,
-					title: {
-						text: 'VolGDP'
-					},
-					opposite: true
-				},
-				{
-					min: 0,
-					title: {
-						text: 'Muertes en carretera pmh'
-					}
 				}
 			],
 			tooltip: {
@@ -541,22 +274,6 @@
 					yAxis: 1,
 					tooltip: {
 						valueSuffix: ' personas'
-					}
-				},
-				{
-					name: 'VolGDP',
-					data: datos.series[2].data, // Nueva serie para VolGDP
-					yAxis: 2, // Usar el tercer eje y para VolGDP
-					tooltip: {
-						valueSuffix: ' unidades'
-					}
-				},
-				{
-					name: 'Muertes en carretera pmh',
-					data: datos.series[3].data, 
-					yAxis: 3, 
-					tooltip: {
-						valueSuffix: ' fallecidos'
 					}
 				}
 			]
--- index.js
+++ index.js
@@ -5,17 +5,12 @@ import {API_TLR_v1} from "./api/api-TLR-v1.js";
 import {API_TLR_v2} from "./api/api-TLR-v2.js";
 import {backend_MRF_v1} from "./api/api-MRF_v1.js";
 import {backend_MRF_v2} from "./api/api-MRF_v2.js";
-import {API_ASC_v1} from "./api/api-ASC_v1.js";
-import {API_ASC_v2} from "./api/api-ASC_v2.js";
-import {API_ASB_v1} from "./api/api-ASB_v1.js";
-import {API_ASB_v2} from "./api/api-ASB_v2.js";
+
 
 //neDB
 import dataStore from "nedb";
 let db_TLR = new dataStore();
-let db_ASC = new dataStore();
 let db_MRF = new dataStore();
-let db_ASB = new dataStore();
 
 //Adaptador Svelte
 import {handler} from "./front/build/handler.js";
@@ -36,10 +31,7 @@ API_TLR_v1(app,db_TLR);
 API_TLR_v2(app,db_TLR);
 backend_MRF_v1(app, db_MRF);
 backend_MRF_v2(app, db_MRF);
-API_ASC_v1(app, db_ASC);
-API_ASC_v2(app, db_ASC);
-API_ASB_v1(app, db_ASB);
-API_ASB_v2(app, db_ASB);
+
 
 //Uso del handler
 
